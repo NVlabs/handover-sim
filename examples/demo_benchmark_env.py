a3509d16eb64dd742799f4b0d7104403e0d68185
Yu-Wei Chao <ychao@nvidia.com>
2022-05-05 14:39:38 -0700

    Add SimplePolicy in demo_benchmark_env.py

--- examples/demo_benchmark_env.py
+++ examples/demo_benchmark_env.py
@@ -1,3 +1,4 @@
+import abc
 import numpy as np
 import pybullet
 
@@ -19,69 +20,82 @@ split = "train"
 scene_id = 105
 
 
-class Policy:
-    def __init__(self, env):
+class SimplePolicy(abc.ABC):
+    def __init__(
+        self,
+        env,
+        time_wait=time_wait,
+        time_action_repeat=time_action_repeat,
+        time_close_gripper=time_close_gripper,
+        back_step_size=back_step_size,
+    ):
         self._env = env
-
         self._steps_wait = int(time_wait / self._env.cfg.SIM.TIME_STEP)
         self._steps_action_repeat = int(time_action_repeat / self._env.cfg.SIM.TIME_STEP)
         self._steps_close_gripper = int(time_close_gripper / self._env.cfg.SIM.TIME_STEP)
+        self._back_step_size = back_step_size
+
+    def reset(self):
+        self._done = False
+        self._done_frame = None
+        self._done_action = None
+        self._back = None
 
     def forward(self, frame):
         if frame < self._steps_wait:
+            # Wait.
             action = start_conf
-        elif frame < self._steps_wait + len(traj) * self._steps_action_repeat:
-            i = (frame - self._steps_wait) // self._steps_action_repeat
-            action = traj[i]
-        elif (
-            frame
-            < self._steps_wait + len(traj) * self._steps_action_repeat + self._steps_close_gripper
-        ):
-            action = traj[-1].copy()
-            action[7:9] = 0.0
+        elif not self._done:
+            # Approach object until reaching grasp pose.
+            action, done = self.plan(frame)
+            if done:
+                self._done = True
+                self._done_frame = frame + 1
+                self._done_action = action
         else:
-            if (
-                frame
-                == self._steps_wait
-                + len(traj) * self._steps_action_repeat
-                + self._steps_close_gripper
-            ):
-                self._back = self.back()
-            if (
-                frame
-                < self._steps_wait
-                + len(traj) * self._steps_action_repeat
-                + self._steps_close_gripper
-                + len(self._back) * self._steps_action_repeat
-            ):
-                i = (
-                    frame
-                    - self._steps_wait
-                    - len(traj) * self._steps_action_repeat
-                    - self._steps_close_gripper
-                ) // self._steps_action_repeat
-                action = self._back[i]
+            # Close gripper and back out.
+            if frame < self._done_frame + self._steps_close_gripper:
+                action = self._done_action.copy()
+                action[7:9] = 0.0
             else:
-                action = self._back[-1]
+                if self._back is None:
+                    self._back = []
+                    pos = self._env.panda.body.link_state[0][self._env.panda.LINK_IND_HAND][
+                        0:3
+                    ].numpy()
+                    dpos_goal = self._env.goal_center - pos
+                    dpos_step = dpos_goal / np.linalg.norm(dpos_goal) * self._back_step_size
+                    num_steps = int(np.ceil(np.linalg.norm(dpos_goal) / self._back_step_size))
+                    for _ in range(num_steps):
+                        pos += dpos_step
+                        conf = pybullet.calculateInverseKinematics(
+                            self._env.panda.body.contact_id[0],
+                            self._env.panda.LINK_IND_HAND - 1,
+                            pos,
+                        )
+                        conf = np.array(conf, dtype=np.float32)
+                        conf[7:9] = 0.0
+                        self._back.append(conf)
+
+                i = (
+                    frame - self._done_frame - self._steps_close_gripper
+                ) // self._steps_action_repeat
+                i = min(i, len(self._back) - 1)
+                action = self._back[i]
 
         return action
 
-    def back(self):
-        back = []
-        pos = self._env.panda.body.link_state[0][self._env.panda.LINK_IND_HAND][0:3].numpy()
-        vec_goal = self._env.goal_center - pos
-        vec_step = vec_goal / np.linalg.norm(vec_goal) * back_step_size
-        num_steps = int(np.ceil(np.linalg.norm(vec_goal) / back_step_size))
-        for _ in range(num_steps):
-            pos += vec_step
-            conf = pybullet.calculateInverseKinematics(
-                self._env.panda.body.contact_id[0], self._env.panda.LINK_IND_HAND - 1, pos
-            )
-            conf = np.array(conf, dtype=np.float32)
-            conf[7:9] = 0.0
-            back.append(conf)
+    @abc.abstractmethod
+    def plan(self, frame):
+        """ """
 
-        return back
+
+class DemoPolicy(SimplePolicy):
+    def plan(self, frame):
+        i = (frame - self._steps_wait) // self._steps_action_repeat
+        action = traj[i]
+        done = frame == self._steps_wait + len(traj) * self._steps_action_repeat - 1
+        return action, done
 
 
 def main():
@@ -94,10 +108,11 @@ def main():
 
     env = HandoverBenchmarkEnv(cfg)
 
-    pi = Policy(env)
+    pi = DemoPolicy(env)
 
     while True:
         env.reset(scene_id=scene_id)
+        pi.reset()
         frame = 0
 
         while True:
