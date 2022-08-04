af0d4f70f502b8ed99e8323e3aae76da5c720a1e
Yu-Wei Chao <ychao@nvidia.com>
2022-08-04 14:22:51 -0700

    Integrate HandoverHandCameraPointStateEnv and PandaHandCamera with easysim camera rendering

--- handover/handover_env.py
+++ handover/handover_env.py
@@ -322,45 +322,7 @@ class HandoverStateEnv(HandoverEnv):
         return {}
 
 
-import pybullet
-
-from scipy.spatial.transform import Rotation as Rot
-
-
 class HandoverHandCameraPointStateEnv(HandoverEnv):
-    def init(self):
-        super().init()
-
-        # Create projection matrix.
-        self._width = 224
-        self._height = 224
-        self._fov = 90.0
-        self._aspect = self._width / self._height
-        self._near = 0.035
-        self._far = 2.0
-        self._projection_matrix = pybullet.computeProjectionMatrixFOV(
-            self._fov, self._aspect, self._near, self._far
-        )
-
-        # Get deproject points before depth multiplication.
-        K = np.eye(3, dtype=np.float32)
-        K[0, 0] = self._width * self._projection_matrix[0] / 2
-        K[1, 1] = self._height * self._projection_matrix[5] / 2
-        K[0, 2] = self._width / 2
-        K[1, 2] = self._height / 2
-        K_inv = np.linalg.inv(K)
-        x, y = np.meshgrid(np.arange(self._width), np.arange(self._height))
-        x = x.astype(np.float32)
-        y = y.astype(np.float32)
-        ones = np.ones((self._height, self._width), dtype=np.float32)
-        xy1s = np.stack((x, y, ones), axis=2).reshape(self._width * self._height, 3).T
-        self._deproject_p = np.matmul(K_inv, xy1s).T
-
-        # Get transform from hand to pinhole camera frame.
-        pos = (+0.036, 0.0, +0.036)
-        orn = Rot.from_euler("XYZ", (0.0, 0.0, +np.pi / 2)).as_quat().astype(np.float32)
-        self._t3d_hand_to_camera = get_t3d_from_qt(orn, pos)
-
     def _get_panda_cls(self):
         return PandaHandCamera
 
@@ -392,44 +354,8 @@ class HandoverHandCameraPointStateEnv(HandoverEnv):
         return {}
 
     def _get_point_state(self):
-        if self._point_state is not None:
-            return self._point_state
-
-        # Get OpenGL view frame from URDF camera frame.
-        pos = self.panda.body.link_state[0, self.panda.LINK_IND_CAMERA, 0:3]
-        orn = self.panda.body.link_state[0, self.panda.LINK_IND_CAMERA, 3:7]
-        R = np.matmul(
-            Rot.from_quat(orn).as_matrix().astype(np.float32),
-            Rot.from_euler("XYZ", (-np.pi / 2, 0.0, -np.pi)).as_matrix().astype(np.float32),
-        )
-
-        # Create view matrix.
-        view_matrix = np.eye(4, dtype=np.float32)
-        view_matrix[:3, :3] = R.T
-        view_matrix[:3, 3] = -1 * np.matmul(R.T, pos)
-        view_matrix = view_matrix.T
-
-        # Render camera image.
-        _, _, _, depth, mask = pybullet.getCameraImage(
-            self._width,
-            self._height,
-            viewMatrix=tuple(view_matrix.flatten()),
-            projectionMatrix=self._projection_matrix,
-            renderer=pybullet.ER_BULLET_HARDWARE_OPENGL,
-        )
-
-        # Convert depth buffer to real depth.
-        depth = self._far * self._near / (self._far - (self._far - self._near) * depth)
-
-        # Get point state in pinhole camera frame.
-        mask = mask == self.ycb.bodies[self.ycb.ids[0]].contact_id[0]
-        point_state = (
-            np.tile(depth[mask].reshape(-1, 1), (1, 3)) * self._deproject_p[mask.ravel(), :]
-        )
-
-        # Transform point state to hand frame.
-        point_state = self._t3d_hand_to_camera.transform_points(point_state)
-
-        self._point_state = point_state
-
+        if self._point_state is None:
+            self._point_state = self.panda.get_point_state(
+                self.ycb.bodies[self.ycb.ids[0]].contact_id[0]
+            )
         return self._point_state
--- handover/panda.py
+++ handover/panda.py
@@ -11,6 +11,12 @@ https://github.com/liruiw/OMG-Planner/blob/dcbbb8279570cd62cf7388bf393c8b3e2d568
 
 import easysim
 import os
+import numpy as np
+import torch
+
+from scipy.spatial.transform import Rotation as Rot
+
+from handover.transform3d import get_t3d_from_qt
 
 
 class Panda:
@@ -66,3 +72,85 @@ class PandaHandCamera(Panda):
     _RIGID_SHAPE_COUNT = 12
 
     LINK_IND_CAMERA = 11
+
+    def __init__(self, cfg, scene):
+        super().__init__(cfg, scene)
+
+        camera = easysim.Camera()
+        camera.name = "panda_hand_camera"
+        camera.width = 224
+        camera.height = 224
+        camera.vertical_fov = 90
+        camera.near = 0.035
+        camera.far = 2.0
+        camera.position = [(0.0, 0.0, 0.0)]
+        camera.orientation = [(0.0, 0.0, 0.0, 1.0)]
+        self._scene.add_camera(camera)
+        self._camera = camera
+
+        # Get rotation from URDF to OpenGL view frame.
+        orn = Rot.from_euler("XYZ", (-np.pi / 2, 0.0, -np.pi)).as_quat().astype(np.float32)
+        self._quat_urdf_to_opengl = torch.from_numpy(orn)
+
+        # Get deproject points before depth multiplication.
+        K = np.eye(3, dtype=np.float32)
+        K[0, 0] = (
+            self._camera.width
+            / 2
+            / np.tan(np.deg2rad(camera.vertical_fov) * self._camera.width / self._camera.height / 2)
+        )
+        K[1, 1] = self._camera.height / 2 / np.tan(np.deg2rad(camera.vertical_fov) / 2)
+        K[0, 2] = self._camera.width / 2
+        K[1, 2] = self._camera.height / 2
+        K_inv = np.linalg.inv(K)
+        x, y = np.meshgrid(np.arange(self._camera.width), np.arange(self._camera.height))
+        x = x.astype(np.float32)
+        y = y.astype(np.float32)
+        ones = np.ones((self._camera.height, self._camera.width), dtype=np.float32)
+        xy1s = np.stack((x, y, ones), axis=2).reshape(self._camera.width * self._camera.height, 3).T
+        self._deproject_p = np.matmul(K_inv, xy1s).T
+
+        # Get transform from hand to pinhole camera frame.
+        pos = (+0.036, 0.0, +0.036)
+        orn = Rot.from_euler("XYZ", (0.0, 0.0, +np.pi / 2)).as_quat().astype(np.float32)
+        self._t3d_hand_to_camera = get_t3d_from_qt(orn, pos)
+
+    def get_point_state(self, segmentation_id):
+        # Get OpenGL view frame from URDF camera frame.
+        pos = self.body.link_state[0, self.LINK_IND_CAMERA, 0:3]
+        orn = self.body.link_state[0, self.LINK_IND_CAMERA, 3:7]
+        orn = _quaternion_multiplication(orn, self._quat_urdf_to_opengl)
+
+        # Set camera pose.
+        self._camera.update_attr_array("position", torch.tensor([0]), pos)
+        self._camera.update_attr_array("orientation", torch.tensor([0]), orn)
+
+        # Render camera image.
+        depth = self._camera.depth[0].numpy()
+        segmentation = self._camera.segmentation[0].numpy()
+
+        # Get point state in pinhole camera frame.
+        segmentation = segmentation == segmentation_id
+        point_state = (
+            np.tile(depth[segmentation].reshape(-1, 1), (1, 3))
+            * self._deproject_p[segmentation.ravel(), :]
+        )
+
+        # Transform point state to hand frame.
+        point_state = self._t3d_hand_to_camera.transform_points(point_state)
+
+        return point_state
+
+
+def _quaternion_multiplication(q1, q2):
+    q1x, q1y, q1z, q1w = torch.unbind(q1, axis=-1)
+    q2x, q2y, q2z, q2w = torch.unbind(q2, axis=-1)
+    return torch.stack(
+        (
+            q1w * q2x + q1x * q2w + q1y * q2z - q1z * q2y,
+            q1w * q2y - q1x * q2z + q1y * q2w + q1z * q2x,
+            q1w * q2z + q1x * q2y - q1y * q2x + q1z * q2w,
+            q1w * q2w - q1x * q2x - q1y * q2y - q1z * q2z,
+        ),
+        axis=-1,
+    )
