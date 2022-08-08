d1182fb3715e631919f1812b9070d5ef468a1a0e
Yu-Wei Chao <ychao@nvidia.com>
2022-08-08 12:51:13 -0700

    Move goal body making to _draw_goal_init() in benchmark wrapper

--- handover/benchmark_wrapper.py
+++ handover/benchmark_wrapper.py
@@ -21,14 +21,17 @@ class HandoverStatusWrapper(easysim.SimulatorWrapper):
         self._max_episode_steps = self.cfg.BENCHMARK.MAX_EPISODE_TIME / self.cfg.SIM.TIME_STEP
 
         if self.cfg.BENCHMARK.DRAW_GOAL:
-            body = easysim.Body()
-            body.name = "goal"
-            body.geometry_type = easysim.GeometryType.SPHERE
-            body.sphere_radius = self.cfg.BENCHMARK.GOAL_RADIUS
-            body.initial_base_position = self.cfg.BENCHMARK.GOAL_CENTER + (0.0, 0.0, 0.0, 1.0)
-            body.link_color = [self.cfg.BENCHMARK.GOAL_COLOR]
-            body.link_collision_filter = [0]
-            self.scene.add_body(body)
+            self._draw_goal_init()
+
+    def _draw_goal_init(self):
+        body = easysim.Body()
+        body.name = "goal"
+        body.geometry_type = easysim.GeometryType.SPHERE
+        body.sphere_radius = self.cfg.BENCHMARK.GOAL_RADIUS
+        body.initial_base_position = self.cfg.BENCHMARK.GOAL_CENTER + (0.0, 0.0, 0.0, 1.0)
+        body.link_color = [self.cfg.BENCHMARK.GOAL_COLOR]
+        body.link_collision_filter = [0]
+        self.scene.add_body(body)
 
     def reset(self, env_ids=None, **kwargs):
         observation = super().reset(env_ids=env_ids, **kwargs)
